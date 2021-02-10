91d19c7a196c3a733cab6a5594603bc82f31d527
SEscobedo <salvadec@live.com.mx>
2021-02-10 16:34:07 -0300

    Adicionar arquivos de projeto.

---
+++ CrudWPF/Formulario.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CrudWPF
+{
+	/// <summary>
+	/// Interação lógica para Formulario.xam
+	/// </summary>
+	public partial class Formulario : Page
+	{
+		public Guid Id { get; set; }
+	
+		public Formulario( Guid Id )
+		{
+			InitializeComponent();
+
+			this.Id = Id;
+			if(this.Id != Guid.Empty)
+			{
+				using(Model.EmployeeDbEntities db = new Model.EmployeeDbEntities())
+				{
+					var oEmployee = db.Employees.Find(this.Id);
+					txtNome.Text = oEmployee.Nome;
+					txtSobrenome.Text = oEmployee.Sobrenome;
+					txtTelefone.Text = oEmployee.Telefone;
+				}
+			}
+		}
+
+		public void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (Id == Guid.Empty) { 
+				//Novo registro
+				using(Model.EmployeeDbEntities db= new Model.EmployeeDbEntities())
+				{
+					var oEmployee = new Model.Employees();
+					oEmployee.Id = Guid.NewGuid();
+					oEmployee.Nome = txtNome.Text;
+					oEmployee.Sobrenome = txtSobrenome.Text;
+					oEmployee.Telefone = txtTelefone.Text;
+
+					db.Employees.Add(oEmployee);
+					db.SaveChanges();
+
+					MainWindow.StaticMainFrame.Content = new MenuLista();
+				}
+			}
+			else
+			{
+				//Editando registro existente
+				using (Model.EmployeeDbEntities db = new Model.EmployeeDbEntities())
+				{
+					var oEmployee = db.Employees.Find(Id);
+					oEmployee.Nome = txtNome.Text;
+					oEmployee.Sobrenome = txtSobrenome.Text;
+					oEmployee.Telefone = txtTelefone.Text;
+
+					db.Entry(oEmployee).State = System.Data.Entity.EntityState.Modified;
+					db.SaveChanges();
+
+					MainWindow.StaticMainFrame.Content = new MenuLista();
+				}
+			}
+		}
+	}
+}
---
+++ CrudWPF/MenuLista.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CrudWPF
+{
+	/// <summary>
+	/// Interação lógica para MenuLista.xam
+	/// </summary>
+	public partial class MenuLista : Page
+	{
+		public MenuLista()
+		{
+			InitializeComponent();
+			Refresh();
+		}
+
+		private void Refresh()
+		{
+			List<EmployeeViewModel> lst = new List<EmployeeViewModel>();
+			using (Model.EmployeeDbEntities db = new Model.EmployeeDbEntities())
+			{
+				lst = (from d in db.Employees
+					   select new EmployeeViewModel
+					   {
+						   Nome = d.Nome,
+						   Sobrenome = d.Sobrenome,
+						   Telefone = d.Telefone,
+						   Id = d.Id
+					   }).ToList();
+
+			}
+			DG.ItemsSource = lst;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			
+			MainWindow.StaticMainFrame.Content = new Formulario(Guid.Empty);
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			Guid id = (Guid)((Button)sender).CommandParameter;
+
+			using (Model.EmployeeDbEntities db = new Model.EmployeeDbEntities())
+			{
+				var oEmployee = db.Employees.Find(id);
+				db.Employees.Remove(oEmployee);
+				db.SaveChanges();
+			}
+
+			Refresh();
+		}
+
+		private void Button_Edit(object sender, RoutedEventArgs e)
+		{
+			Guid id = (Guid)((Button)sender).CommandParameter;
+
+			Formulario pFormulario = new Formulario(id);
+
+			MainWindow.StaticMainFrame.Content = pFormulario;
+
+		}
+	}
+
+	public class EmployeeViewModel
+	{
+		public Guid Id { get; set; }
+		public string Nome { get; set; }
+		public string Sobrenome { get; set; }
+		public string Telefone { get; set; }
+	}
+}
